6ee51839c9b622fa88972f2a7646ba04c2318e81
Jon Huber <hubermjonathan@gmail.com>
2022-05-12 21:23:44 -0700

    Initial Commit

---
+++ Shared/CookbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookbookApp.swift
+//  Shared
+//
+//  Created by Jonathan Huber on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CookbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
